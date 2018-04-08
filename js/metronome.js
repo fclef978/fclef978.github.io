a3e419148c990dc9b43f0b0a1dbdf70e46bfde40
fclef978 <fclef978@icloud.com>
2018-04-08 14:00:22 +0900

    up

--- js/metronome.js
+++ js/metronome.js
@@ -1,27 +1,15 @@
-var p1 = new Audio('../sample/bell.wav');
-var p4 = new Audio('../sample/tick.wav');
-var p8 = new Audio('../sample/tickL.wav');
-var p12 = new Audio('../sample/tickL.wav');
-var p16 = new Audio('../sample/tickL.wav');
+var p1 = new Audio('sample/bell.wav');
+var p4 = new Audio('sample/tick.wav');
+var p8 = new Audio('sample/tickL.wav');
+var p12 = new Audio('sample/tickL.wav');
+var p16 = new Audio('sample/tickL.wav');
 
-function metronome(tempo, beat) {
-    var lamp = $('#lamp');
-
-    var i = 0;
-    return setInterval(function () {
-        if (i % (12 * beat) == 0) p1.play();
-        if (i % 12 == 0) p4.play();
-        else if (i % 6 == 0) p8.play();
-        else if (i % 4 == 0) audioRestart(p12);
-        else if (i % 3 == 0) audioRestart(p16);
-
-        if (i % 12 == 0) lamp.fadeIn(100, function () {
-            $(this).fadeOut(100)
-        });
-
-        i++;
-    }, 20 / tempo * 250);
-}
+var running = false;
+var volumes = [1.0, 1.0, 1.0, 0.5, 0.0, 0.0];
+var datas = [
+    '#main-volume', '#volume1', '#volume4', '#volume8', '#volume12', '#volume16'
+];
+var count = 0, tempo = 120, beat = 4;
 
 function audioStop(audio) {
     audio.pause();
@@ -33,47 +21,35 @@ function audioRestart(audio) {
     audio.play();
 }
 
-var timer;
-var running = false;
-
 $(function () {
     $('#start-stop').click(function () {
         if (running) {
-            clearInterval(timer);
             running = false;
         }
         else {
-            timer = metronome($('#knob').val(), 4);
             running = true;
+            setVolume();
+            metronome();
         }
     });
 });
 
-var volumes = new Array(6);
-volumes.fill(1.0);
-var datas = [
-    '#main-volume', '#volume1', '#volume4', '#volume8', '#volume12', '#volume16'
-];
-
 $(function () {
-    $.each(datas, function (key, str) {
-        $(str).slider({
-            orientation: 'vertical', min: -2, max: 10, value: 10,
-            slide: function () {
-                volumes[key] = $(str).slider('value') / 10.0;
-                setVolume();
-            }
-        });
+    $.each(datas, function (key, val) {
+        $(val).attr({value: volumes[key] * 20, min: 0, max: 20, step: 1});
+        $(val).on('input', function () {
+            volumes[key] = ($(this).val() / 20);
+            setVolume();
+        })
     });
-    $('#knob').knob({
-        min: 40,
-        max: 240,
-        width: "150",
-        cursor: true,
-        thickness: .3,
-        fgColor: "#222222"
+    var knob = $('#knob');
+    knob.knob({
+        min: 40, max: 240, width: "150", cursor: true, thickness: .3, fgColor: "#222222",
+        change: function (val) {
+            tempo = val;
+        }
     });
-    $('#knob').val(120).trigger('change');
+    knob.val(120).trigger('change');
 });
 
 function setVolume() {
@@ -87,4 +63,31 @@ function setVolume() {
 function zero(num) {
     if (num < 0) num = 0;
     return num;
+}
+
+function metronome() {
+    var waitFunc = function () {
+        if (!running) return;
+        ring(count);
+        count++;
+        clearTimeout(id);
+        id = setTimeout(waitFunc,calcPeriod(tempo));
+    };
+    var id = setTimeout(waitFunc, calcPeriod(tempo));
+}
+
+function ring(i) {
+    if (i % (12 * beat) == 0) p1.play();
+    if (i % 12 == 0) p4.play();
+    else if (i % 6 == 0) p8.play();
+    else if (i % 4 == 0) audioRestart(p12);
+    else if (i % 3 == 0) audioRestart(p16);
+
+    if (i % 12 == 0) $('#lamp').fadeIn(10, function () {
+        $(this).fadeOut(100)
+    });
+}
+
+function calcPeriod(tempo) {
+    return 20 / tempo * 250;
 }
